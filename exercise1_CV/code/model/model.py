15a6d2b2626011ae301834f1fbd754c9e5bc0512
Kirill Yankov <myworkpostbox@gmail.com>
2019-05-09 12:25:04 +0200

    training v30

--- exercise1_CV/code/model/model.py
+++ exercise1_CV/code/model/model.py
@@ -41,7 +41,7 @@ class SoftResNetModel(nn.Module):
     def __init__(self, pretrained):
         super().__init__()
         self.res_conv = ResNetConv(BasicBlock, [2, 2, 2, 2])
-        self.avgpool = nn.Conv2d(in_channels=256, out_channels=17, kernel_size=2, padding=0)
+        self.avgpool = nn.Conv2d(in_channels=256, out_channels=17, kernel_size=1, padding=0)
         self.ups = nn.Upsample((256, 256))
         self.argm = SoftArgmax()
 
