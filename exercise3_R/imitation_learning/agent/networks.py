65e96dc567bf6313d17dc5aa14da108419e3bcdd
Kirill Yankov <myworkpostbox@gmail.com>
2019-06-04 02:30:33 +0200

    task3

--- exercise3_R/imitation_learning/agent/networks.py
+++ exercise3_R/imitation_learning/agent/networks.py
@@ -18,9 +18,9 @@ class CNN(nn.Module):
         # self.linear = torch.nn.Linear(in_features=5*5, out_features=n_classes)
         self.cnn1 = torch.nn.Conv2d(in_channels=history_length, out_channels=history_length, kernel_size=3)
         self.sbs1 = torch.nn.MaxPool2d(kernel_size=4)
-        self.lstm = torch.nn.LSTM(input_size=23*23, hidden_size=30*30, batch_first=True)
+        self.lstm = torch.nn.LSTM(input_size=23*23, hidden_size=15*15, dropout=0.2, batch_first=True)
         self.sbs2 = torch.nn.AvgPool2d(kernel_size=4, stride=2)
-        self.linear = torch.nn.Linear(in_features=14*14, out_features=n_classes)
+        self.linear = torch.nn.Linear(in_features=6*6, out_features=n_classes)
 
 
     def forward(self, x):
@@ -29,7 +29,7 @@ class CNN(nn.Module):
         x = self.sbs1(x)
         x, _ = self.lstm(x.view(x.shape[0], x.shape[1], -1))
         # x = self.sbs2(x.view(x.shape[0], x.shape[1], 10, 10))
-        x = self.sbs2(x.view(x.shape[0], x.shape[1], 30, 30))
+        x = self.sbs2(x.view(x.shape[0], x.shape[1], 15, 15))
         x = self.linear(x.view(x.shape[0], x.shape[1], -1))
         return x
 
