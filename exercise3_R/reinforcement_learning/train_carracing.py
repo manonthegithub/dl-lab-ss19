be5f26727ad4953852f5f44905a93bc69fd09883
Kirill Yankov <myworkpostbox@gmail.com>
2019-06-04 02:13:36 +0200

    task3

--- exercise3_R/reinforcement_learning/train_carracing.py
+++ exercise3_R/reinforcement_learning/train_carracing.py
@@ -45,7 +45,7 @@ def run_episode(env, agent, deterministic, skip_frames=5,  do_training=True, ren
         # Hint: adapt the probabilities of the 5 actions for random sampling so that the agent explores properly. 
         # action_id = agent.act(...)
         # action = your_id_to_action_method(...)
-        action_id = agent.act(state=state, deterministic=deterministic, p=[0.35, 0.2, 0.2, 0.5, 0.2])
+        action_id = agent.act(state=state, deterministic=deterministic, p=[0.35, 0.2, 0.2, 0.05, 0.2])
         action = id_to_action(action_id)
         actions[action_id] += 1
 
@@ -155,7 +155,7 @@ if __name__ == "__main__":
     # ...
     Q = CNN(history_length=hl + 1, n_classes=num_actions)
     Q_target = CNN(history_length=hl + 1, n_classes=num_actions)
-    agent = DQNAgent(Q, Q_target, num_actions, gamma=0.95, batch_size=64, epsilon=0.5, tau=0.01, lr=1e-4)
+    agent = DQNAgent(Q, Q_target, num_actions, gamma=0.8, batch_size=64, epsilon=0.5, tau=0.01, lr=1e-4)
     fn = os.path.join(model_dir, 'dqn_agent.ckpt')
     if os.path.exists(fn):
         agent.load(fn)
