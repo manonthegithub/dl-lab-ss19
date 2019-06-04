69a00c22adb2194a34115afa7b11d362870e6211
Kirill Yankov <myworkpostbox@gmail.com>
2019-06-04 11:07:40 +0200

    task3

--- exercise3_R/imitation_learning/agent/networks.py
+++ exercise3_R/imitation_learning/agent/networks.py
@@ -21,7 +21,7 @@ class CNN(nn.Module):
         self.sbs1 = torch.nn.MaxPool2d(kernel_size=4, stride=2)
         self.lstm = torch.nn.LSTM(input_size=46*46, hidden_size=15*15, batch_first=True)
         self.sbs2 = torch.nn.AvgPool2d(kernel_size=2, stride=1)
-        self.drp2 = torch.nn.Dropout()
+        self.drp2 = torch.nn.Dropout(0.2)
         self.linear = torch.nn.Linear(in_features=14*14, out_features=n_classes)
 
 
