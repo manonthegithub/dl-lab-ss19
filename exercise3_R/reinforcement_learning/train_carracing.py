d63fafb2ef7a0b6c55f3259c8040401fe0276f85
Kirill Yankov <myworkpostbox@gmail.com>
2019-06-04 17:02:56 +0200

    task3

--- exercise3_R/reinforcement_learning/train_carracing.py
+++ exercise3_R/reinforcement_learning/train_carracing.py
@@ -45,7 +45,7 @@ def run_episode(env, agent, deterministic, skip_frames=0,  do_training=True, ren
         # Hint: adapt the probabilities of the 5 actions for random sampling so that the agent explores properly. 
         # action_id = agent.act(...)
         # action = your_id_to_action_method(...)
-        action_id = agent.act(state=state, deterministic=deterministic, p=[0.4, 0.2, 0.2, 0.05, 0.15])
+        action_id = agent.act(state=state, deterministic=deterministic, p=[0.3, 0.2, 0.2, 0.15, 0.15])
         action = id_to_action(action_id, 0.7, True)
         actions[action_id] += 1
 
